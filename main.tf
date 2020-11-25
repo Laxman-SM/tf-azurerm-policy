1c6e4bca80d034b35406ae4ae6b46141cd471c88
Laxman Singh <laxmansingh_m@hotmail.com>
2020-11-26 00:13:10 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -37,7 +37,13 @@ resource "azurerm_policy_definition" "auditRoleAssignmentType_user" {
   mode         = "All"
   display_name = "Audit user role assignments"
   description  = "This policy checks for any Role Assignments of Type [User] - useful to catch individual IAM assignments to resources/RGs which are out of compliance with the RBAC standards e.g. using Groups for RBAC."
+  metadata = <<METADATA
+    {
+    "category": "${var.policyset_definition_category}"
+    }
+METADATA
   policy_rule = <<POLICY_RULE
+
     {
     "if": {
       "allOf": [
@@ -56,8 +62,6 @@ resource "azurerm_policy_definition" "auditRoleAssignmentType_user" {
     }
   }
 POLICY_RULE
-
-
   parameters = <<PARAMETERS
     {
     "principalType": {
