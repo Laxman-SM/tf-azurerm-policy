3f032d1349c87e37acc43d39032aed8c7781798e
Laxman Singh <laxmansingh_m@hotmail.com>
2020-11-25 15:27:40 +0530

    syntex update

--- main.tf
+++ main.tf
@@ -88,6 +88,6 @@ PARAMETERS
 }
 
 output "auditRoleAssignmentType_user_policy_id" {
-  value       = "${azurerm_policy_definition.auditRoleAssignmentType_user.id}"
+  value       = "azurerm_policy_definition.auditRoleAssignmentType_user.id"
   description = "The policy definition id for auditRoleAssignmentType_user"
 }
