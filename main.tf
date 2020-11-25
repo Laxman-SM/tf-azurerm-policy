d4a5552a6fde65cc71cec882093d2a05c74198c1
Laxman Singh Mandloi <Laxman-SM@users.noreply.github.com>
2020-11-26 00:03:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,13 +37,6 @@ resource "azurerm_policy_definition" "auditRoleAssignmentType_user" {
   mode         = "All"
   display_name = "Audit user role assignments"
   description  = "This policy checks for any Role Assignments of Type [User] - useful to catch individual IAM assignments to resources/RGs which are out of compliance with the RBAC standards e.g. using Groups for RBAC."
-
-  metadata = <<METADATA
-    {
-    "category": "$(var.policy_definition_category}",
-    "version" : "1.0.0"
-METADATA
-
   policy_rule = <<POLICY_RULE
     {
     "if": {
