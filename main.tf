58dd275b07e63d0767cde0f1a5f9daa0e4cbcce1
Laxman Singh Mandloi <Laxman-SM@users.noreply.github.com>
2020-11-25 18:57:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -91,3 +91,4 @@ output "auditRoleAssignmentType_user_policy_id" {
   value       = azurerm_policy_definition.auditRoleAssignmentType_user.id
   description = "The policy definition id for auditRoleAssignmentType_user"
 }
+
