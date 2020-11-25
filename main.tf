9668fa5197bad50dc2e2c3d1905958674f67d4f5
Laxman Singh <laxmansingh_m@hotmail.com>
2020-11-25 20:43:32 +0530

    output commented

--- main.tf
+++ main.tf
@@ -87,8 +87,11 @@ PARAMETERS
 
 }
 
+/**
+
 output "auditRoleAssignmentType_user_policy_id" {
   value       = azurerm_policy_definition.auditRoleAssignmentType_user.id
   description = "The policy definition id for auditRoleAssignmentType_user"
 }
 
+**/
