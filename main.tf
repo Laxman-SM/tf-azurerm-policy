996bb2a156e0ade6eba13f3260c133350eac5b67
Laxman Singh <laxmansingh_m@hotmail.com>
2020-11-26 00:35:42 +0530

    parameter to AzurmRM policyset resource

--- main.tf
+++ main.tf
@@ -25,6 +25,7 @@ provider "azurerm" {
   features {}
 }
 
+/**
 variable "policy_definition_category" {
   type        = string
   description = "The category to use for all Policy Definitions"
@@ -85,4 +86,55 @@ PARAMETERS
 output "auditRoleAssignmentType_user_policy_id" {
   value       = azurerm_policy_definition.auditRoleAssignmentType_user.id
   description = "The policy definition id for auditRoleAssignmentType_user"
+}
+
+**/
+
+########################################################################################
+
+# passing array parameter to AzureRM policyset resource 
+
+variable "policyset_definitions" {
+  type        = list
+  description = "array of built-in policy definitions"
+  default = [
+    "Allowed locations for resource groups"
+  ]
+}
+
+data "azurerm_policy_definition" "policyset_definitions" {
+  count        = length(var.policyset_definitions)
+  display_name = var.policyset_definitions[count.index]
+}
+
+resource "azurerm_policy_set_definition" "custompolicyset" {
+  name         = "CustomPolicySet"
+  policy_type  = "Custom"
+  display_name = "CustomPolicySet"
+
+  parameters = <<PARAMETERS
+    {
+        "allowedLocations": {
+            "type": "Array",
+            "metadata": {
+                "description": "The list of locations that resource groups can be created in.",
+                "displayName": "Allowed locations",
+                "strongType": "location"
+            },
+            "defaultValue": [
+                "australiaeast",
+                "australiasoutheast"
+                ]
+        }
+    }
+PARAMETERS
+
+  policy_definition_reference {
+    policy_definition_id = data.azurerm_policy_definition.policyset_definitions.*.id[0]
+    parameter_values     = <<VALUE
+    {
+      "listOfAllowedLocations": {"value": "[parameters('allowedLocations')]"}
+    }
+    VALUE
+  }
 }
